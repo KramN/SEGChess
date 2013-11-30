433549345b303e5abfc8264684909932f50bf6a4
pshortt <pshortt21@gmail.com>
2013-11-30 16:23:05 -0500

    dem comments

--- src/general/Square.java
+++ src/general/Square.java
@@ -1,7 +1,10 @@
 package general;
 
 import java.io.Serializable;
-
+/**
+ * 
+ * Represents the nodes that make up the board of the game.
+ */
 public class Square implements Serializable {
 	
 	private static final long serialVersionUID = 6855502952320772867L;
@@ -9,31 +12,42 @@ public class Square implements Serializable {
 	//------------------------
 	// MEMBER VARIABLES
 	//------------------------
+	//Square Attributes
 	private boolean hasPiece;
 	
 	//Square Associations
 	private SpecificPiece thePiece;
-
+	
+	//------------------------
+	// CONSTRUCTOR
+	//------------------------
 	public Square(){
 		hasPiece = false;
 	}
 	
+	//GETTERS
+	public boolean hasPiece(){
+		return hasPiece;
+	}
+	
+	//SETTERS
 	public void setPiece(SpecificPiece piece){
 		//handle the case when there is already a piece occupying the square
 		thePiece = piece;
 		hasPiece = true;
 	}
 	
+	//INSTANCE METHODS
+	
+	/**
+	 * Removes the piece from this square.
+	 */
 	public SpecificPiece removePiece(){
-		//Check for no piece existing. Throw exception if no piece.
+		//TODO: Check for no piece existing. Throw exception if no piece.
 		SpecificPiece temp = thePiece;
 		thePiece = null;
 		hasPiece = false;
 		return temp;
-	}
-	
-	public boolean hasPiece(){
-		return hasPiece;
 	}
 	
 	public String toString(){
